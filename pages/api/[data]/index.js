22cf0c54caf099a3f2c54a5cb7c992c3384f0a31
TolulopeJoshua <babtol235@gmail.com>
2023-03-24 09:42:45 +0100

    tch

--- pages/api/[data]/index.js
+++ pages/api/[data]/index.js
@@ -5,6 +5,7 @@ export default async function handler(req,  res) {
     if (req.method === 'GET') {
         // originf(req, res);
         await refresher();
+        // console.log(require('/tmp/world.json'))
         res.status(200).send([req.headers.host, req.headers.referer])
     }
 }
--- utils/refresher.js
+++ utils/refresher.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 export default async () => {
