640f25001539ffac1123bd230993464b43bb6e78
TolulopeJoshua <babtol235@gmail.com>
2023-03-24 09:09:01 +0100

    tc

--- pages/api/location.js
+++ pages/api/location.js
@@ -1,16 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import requestIp from 'request-ip'
 import axios from 'axios'
 
-type Data = {
-  name: string
-}
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler( req, res ) {
   const detectedIp = requestIp.getClientIp(req)
   res.status(200).send((await axios.get(`https://ipapi.co/${detectedIp}/json/`)).data.city)
 }
