a39a9e607d790b936e2282523d504bbb26532745
TolulopeJoshua <babtol235@gmail.com>
2023-03-24 09:29:14 +0100

    tch

--- utils/refresher.js
+++ utils/refresher.js
@@ -1,8 +1,8 @@
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 export default async () => {
-    if (!existsSync('newsData')){
-        mkdirSync('newsData');
+    if (!existsSync('tmp')){
+        mkdirSync('tmp');
         const url = `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}.json`
         const newsData = (await axios.get(url)).data;
         for (let section in newsData) {
@@ -10,7 +10,7 @@ export default async () => {
             for (let key in newsData[section]) {
                 sectionData.push(newsData[section][key]);
             }
-            const sectionPath = `newsData/${section}.json`
+            const sectionPath = `tmp/${section}.json`
             writeFileSync(sectionPath, JSON.stringify(sectionData.sort((a,b) => {
                 return (new Date(b.pubDate) - (new Date(a.pubDate)))
             }).slice(0,100)))
