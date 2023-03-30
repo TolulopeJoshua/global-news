a80fcc1ea679c66d4f806e375c622b8c13f2e762
TolulopeJoshua <babtol235@gmail.com>
2023-03-30 09:48:40 +0100

    tt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: ['encrypted-tbn0.gstatic.com', 'openweathermap.org', 'i.ytimg.com']
   }
