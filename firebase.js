014e7997ea4093138cbec63e1716b4f8d7c1db12
TolulopeJoshua <babtol235@gmail.com>
2023-03-31 10:19:55 +0100

    t2

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import { getAuth } from "firebase/auth"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyAJV_d_pxR945hMZJu303ldfIrWsOLns6Q',
-  authDomain: "gipnews.firebaseapp.com",
+  authDomain: "gipnews.vercel.app",
   databaseURL: "https://gipnews-default-rtdb.firebaseio.com",
   projectId: "gipnews",
   storageBucket: "gipnews.appspot.com",
