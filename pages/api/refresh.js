6a7f06e472a25be9324eae6fc1cf3b7e208d7dc5
TolulopeJoshua <babtol235@gmail.com>
2023-04-01 21:14:38 +0100

    rl

--- pages/api/refresh.js
+++ pages/api/refresh.js
@@ -26,20 +26,21 @@ export default async function handler(req,  res) {
         const sectionPath = `/tmp/${section.split(',')[0]}.json`;
         try {
             if (section == 'reel') {
-                const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=25&key=${process.env.NEXT_SECRET_YOUTUBE_API_KEY}`;
-                const response = await axios.get(url)
-                let { items } = response.data;
-                console.log(section, items.length);
-                sectionData = items.map(video => {
+                const ids = [17,24,25,28]
+                const urls = ids.map(id => `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=${id}&key=${process.env.NEXT_SECRET_YOUTUBE_API_KEY}`);
+                const responses = await Promise.all(urls.map(url => axios.get(url))) 
+                let items = responses.reduce((res, sec) => res.concat(sec.data.items), []);
+                sectionData = items.filter(({contentDetails}) => !contentDetails.regionRestriction).map(video => {
                     const {
                         id, snippet: {title, description, publishedAt: pubDate, 
-                        thumbnails:{standard}}, player: {embedHtml: content}
-                    } = video;
-                    return ({title, description, content, pubDate, image_url: standard?.url, id});
-                })
+                            thumbnails:{standard}}, player: {embedHtml: content}
+                        } = video;
+                        return ({title, description, content, pubDate, image_url: standard?.url, id});
+                    })
+                console.log(section, sectionData.length);
                 writeFileSync(sectionPath, JSON.stringify(sectionData.sort((a,b) => {
                     return (new Date(b.pubDate) - (new Date(a.pubDate)))
-                })));
+                }).slice(0,100)));
             } else {
                 try {
                     sectionData = JSON.parse(readFileSync(sectionPath)) || [];
