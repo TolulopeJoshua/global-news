a9214bf81d9ea0928dc8455a1b819aa14a501adb
TolulopeJoshua <babtol235@gmail.com>
2023-04-02 13:19:28 +0100

    ts

--- pages/api/[data]/index.js
+++ pages/api/[data]/index.js
@@ -13,45 +13,53 @@ export default async function handler(req,  res) {
         const data = {};
         for (let section of sections) {
             let sectionData = [];
-            const sectionPath = `/tmp/${section?.split(',')[0]}.json`;
+            let sectionPath = `/tmp/${section?.split(',')[0]}.json`;
             try {
+                // throw ' '
                 sectionData = JSON.parse(readFileSync(sectionPath)) || [];
+                data[section] = sectionData;
             } catch (error) { 
                 console.log('db get');
-                if (section == 'reel') {
-                    const url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=snippet&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=25&key=${process.env.NEXT_SECRET_YOUTUBE_API_KEY}`;
-                    let response = await axios.get(url)
-                    let { items } = response.data;
-                    sectionData = items.map(video => {
-                        const {
-                            id, snippet: {title, description, publishedAt: pubDate, 
-                            thumbnails:{standard}}, player: {embedHtml: content}
-                        } = video;
-                        return ({title, description, content, pubDate, image_url: standard?.url, id});
-                    })
-                    data[section] = sectionData.sort((a,b) => {
-                        return (new Date(b.pubDate) - (new Date(a.pubDate)))
-                    }).map(art => ({...art, section}));
+                const urls = sections.slice(0,7).map(sec => `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${sec.split(',')[0]}.json?orderBy="pubDate"&limitToLast=100`)
+                urls.push(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&part=player&part=snippet&part=contentDetails&part=status&chart=mostPopular&maxResults=50&videoCategoryId=25&key=${process.env.NEXT_SECRET_YOUTUBE_API_KEY}`)
+                // const newsSection = (await axios.get(url)).data;
+                const newsSections = (await Promise.all(urls.map(url => axios.get(url)))).map(res => res.data);
+                for (let newsSection of newsSections.slice(0,7)) {
+                    section = sections[newsSections.indexOf(newsSection)];
+                    sectionPath = `/tmp/${section?.split(',')[0]}.json`;
+                    sectionData = [];
+                    for (let key in newsSection) {
+                        sectionData.push(newsSection[key]);
+                    }
+                    data[section] = sectionData.sort((a,b) => (new Date(b.pubDate) - (new Date(a.pubDate))))
+                        .sort((a,b) => {
+                            const val = (a.image_url && !b.image_url) ? -1 :
+                                        (b.image_url && !a.image_url) ? 1 : 0
+                            return val;
+                        }).map(art => ({...art, section}));
                     try {
                         writeFileSync(sectionPath, JSON.stringify(data[section])); 
                     } catch (error) { console.log('could not write to file') }
-                    continue;
                 }
-                const url = `https://gipnews-default-rtdb.firebaseio.com/${process.env.NEXT_SECRET_FIREBASE_APIKEY}/${section.split(',')[0]}.json?orderBy="pubDate"&limitToLast=100`
-                const newsSection = (await axios.get(url)).data;
-                for (let key in newsSection) {
-                    sectionData.push(newsSection[key]);
-                }
-            }
-            data[section] = sectionData.sort((a,b) => (new Date(b.pubDate) - (new Date(a.pubDate))))
-                .sort((a,b) => {
-                    const val = (a.image_url && !b.image_url) ? -1 :
-                                (b.image_url && !a.image_url) ? 1 : 0
-                    return val;
+                section = 'reel';
+                sectionPath = `/tmp/reel.json`;
+                sectionData = [];
+                let { items } = newsSections[7];
+                sectionData = items.map(video => {
+                    const {
+                        id, snippet: {title, description, publishedAt: pubDate, 
+                        thumbnails:{standard}}, player: {embedHtml: content}
+                    } = video;
+                    return ({title, description, content, pubDate, image_url: standard?.url, id});
+                })
+                data[section] = sectionData.sort((a,b) => {
+                    return (new Date(b.pubDate) - (new Date(a.pubDate)))
                 }).map(art => ({...art, section}));
-            try {
-                writeFileSync(sectionPath, JSON.stringify(data[section])); 
-            } catch (error) { console.log('could not write to file') }
+                try {
+                    writeFileSync(sectionPath, JSON.stringify(data[section]));  
+                } catch (error) { console.log('could not write to file') }
+                break;
+            }
         }
         res.status(200).send(data)
     }
