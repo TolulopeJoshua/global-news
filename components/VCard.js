a2511a49517c0a7c82e1c24dd57281765e1ae5e2
TolulopeJoshua <babtol235@gmail.com>
2023-05-02 17:13:46 +0100

    t

--- components/VCard.js
+++ components/VCard.js
@@ -10,7 +10,7 @@ export default ({video={}, img, title, type=0, cat=''}) => {
         <Image src={video.image_url || img.src} width={300} height={150} alt={video.title || title} className={`border-2 w-full aspect-video bg-slate-300 opacity-70 group-hover:opacity-30 transition-opacity`}  />
         <div className={`font-semibold flex flex-col p-3 gap-2 w-auto ${type ? 'absolute bottom-0 left-0 text-gray-300':'text-gray-600 bg-white h-full'}`}>
           {/* {cat ? <span className='text-sm'>{cat}</span> : ''} */}
-          <span className={type ? 'text-base lg:text-xl overflow-clip':'text-xl overflow-clip'}>{video.title || title}</span>
+          <span className={type ? 'text-base lg:text-xl overflow-clip':'text-lg lg:text-xl overflow-clip'}>{video.title || title}</span>
           {type ? <span className='flex items-center'><span className='pr-2 text-xl text-blue-600'><BsPlayCircleFill /></span><span>Watch now</span></span> : ''}
         </div>
     </a>
